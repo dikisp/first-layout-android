b9a2876170f84039aab8ef10069fda1d58bfc87d
Diki Supriadi <dikisupriadi021@email.unikom.ac.id>
2019-02-28 18:15:32 +0700

    first commit

---
+++ MADE/FirstIntent/app/src/main/java/com/scholarship/made/firstintent/MyFlexibleFragment.java
@@ -0,0 +1,13 @@
+package com.scholarship.made.firstintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFlexibleFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_flexible_fragment);
+    }
+}
