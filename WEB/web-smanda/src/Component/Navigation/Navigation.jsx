f9354fa93a413bc3fc0c04dae77127cd0938274d
Diki Supriadi <dikisupriadi021@email.unikom.ac.id>
2019-02-28 19:04:04 +0700

    hallo

---
+++ WEB/web-smanda/src/Component/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+class Navigation extends Component {
+    render(){
+        return (
+            <h1> Hello World</h1>
+        )           
+            
+    }
+}
+
+export default Navigation;
---
+++ WEB/web-smanda/src/Component/beranda.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import navigation from './Navigation/Navigation'
+
+class beranda extends Component(){
+    render(){
+        return(
+            <div>
+                <h1>
+                    hello world
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default beranda;
